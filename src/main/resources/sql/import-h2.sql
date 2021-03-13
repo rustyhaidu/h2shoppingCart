1a4741e9ed1de2934b45063d36bd7adb2fa2783f
Claudiu Haidu <claudiu.haidu@p3-holding.com>
2021-03-13 11:24:36 +0200

    github

--- src/main/resources/sql/import-h2.sql
+++ src/main/resources/sql/import-h2.sql
@@ -27,22 +27,22 @@ INSERT INTO USER_ROLE (user_id, role_id)
 VALUES (3, 2);
 
 INSERT INTO PRODUCT (name, description, quantity, price)
-VALUES ('Soap', 'Pears baby soap for Kids', 1, 35.75);
+VALUES ('Soap', 'Pears baby soap for Kids', 5000, 35.75);
 INSERT INTO PRODUCT (name, description, quantity, price)
-VALUES ('Tooth Brush', 'Signal Tooth Brushes Size in (L, M, S)', 5, 34.50);
+VALUES ('Tooth Brush', 'Signal Tooth Brushes Size in (L, M, S)', 5000, 34.50);
 INSERT INTO PRODUCT (name, description, quantity, price)
 VALUES ('Shirt', 'Casual Shirt imported from France', 3, 1500.00);
 INSERT INTO PRODUCT (name, description, quantity, price)
-VALUES ('Office Bag', 'Leather bag imported from USA', 40, 1000.00);
+VALUES ('Office Bag', 'Leather bag imported from USA', 4000, 1000.00);
 INSERT INTO PRODUCT (name, description, quantity, price)
-VALUES ('Bottle', 'Hot Water Bottles', 80, 450.45);
+VALUES ('Bottle', 'Hot Water Bottles', 8000, 450.45);
 INSERT INTO PRODUCT (name, description, quantity, price)
 VALUES ('Wrist Watch', 'Imported wrist watches from swiss', 800, 2500.00);
 INSERT INTO PRODUCT (name, description, quantity, price)
-VALUES ('Mobile Phone', '3G/4G capability', 700, 45000.00);
+VALUES ('Mobile Phone', '3G/4G capability', 7000, 45000.00);
 INSERT INTO PRODUCT (name, description, quantity, price)
-VALUES ('Shampoo', 'Head and Shoulders Shampoo', 500, 300.00);
+VALUES ('Shampoo', 'Head and Shoulders Shampoo', 5000, 300.00);
 INSERT INTO PRODUCT (name, description, quantity, price)
 VALUES ('Leather Wallets', 'Imported Leather Wallets from AUS', 1000, 500.00);
 INSERT INTO PRODUCT (name, description, quantity, price)
-VALUES ('Camera', 'Imported Canon camera from USA', 10, 85000.00);
+VALUES ('Camera', 'Imported Canon camera from USA', 1000, 85000.00);
